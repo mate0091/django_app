bf861256ed0bf7fcf31c4fd40695510eb13b1328
mate0091 <mate.kelemen009@gmail.com>
2019-11-04 21:00:24 +0200

    film working

--- django_app/sakila/views.py
+++ django_app/sakila/views.py
@@ -52,11 +52,13 @@ def flm(request):
         if form.is_valid():
             try:
                 form.save()
-                return redirect("/show_film")
+                return redirect("/film_show")
             except:
                 pass
+        else:
+            print(form.errors)
     else:
-        form = ActorForm()
+        form = FilmForm()
 
     return render(request, "film_index.html", {"form": form})
 
@@ -76,7 +78,10 @@ def update_film(request, id):
 
     if form.is_valid():
         form.save()
-        return redirect("/")
+        return redirect("/film_show")
+
+    else:
+        print(form.errors)
 
     return render(request, "film_edit.html", {"film": film})
 
@@ -85,3 +90,42 @@ def destroy_film(request, id):
     film = Film.objects.get(film_id=id)
     film.delete()
     return redirect("/film_show")
+
+def flmac(request):
+    if request.method == "POST":
+        form = FilmActorForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect("/filmactor_show")
+            except:
+                pass
+        else:
+            print(form.errors)
+    else:
+        form = FilmForm()
+
+    return render(request, "filmactor_index.html", {"form": form})
+
+def show_film(request):
+    filmactors = FilmActor.objects.all()
+    return render(request, "film_show.html", {"film": filmactors})
+
+def edit_film(request, id1, id2):
+
+    filmactor = FilmActor.objects.get(film_id=id)
+    return render(request, "filmactor_edit.html", {"film": filmactor})
+
+def update_film(request, id1, id2):
+
+    film = Film.objects.get(film_id=id)
+    form = FilmForm(request.POST, instance=film)
+
+    if form.is_valid():
+        form.save()
+        return redirect("/film_show")
+
+    else:
+        print(form.errors)
+
+    return render(request, "film_edit.html", {"film": film})
